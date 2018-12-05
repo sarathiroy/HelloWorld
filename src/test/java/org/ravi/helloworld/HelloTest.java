0ed8a4fc93eb7bcdbf84f649ca850d32b3993e9e
Roy <sarathi.roy@accenture.com>
2018-12-05 16:21:04 +0530

    Modified 3 HelloTest.java

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -15,16 +15,16 @@ import org.testng.annotations.AfterTest;
 
 public class HelloTest {
 
-	WebDriver driver;
+	/*WebDriver driver;
 	WebElement text;
 	//@BeforeTest
 	public void beforeTest() {
-		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+			System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
 		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
 	//@Test
@@ -39,7 +39,7 @@ public class HelloTest {
 	//@AfterTest
 	public void afterTest() {
 		//driver.close();
-	}
+	}*/
 
 
 }
