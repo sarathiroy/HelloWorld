e2e8649aa9d07d503c9d3f72e19fa308b321b17e
Roy <sarathi.roy@accenture.com>
2018-12-06 15:08:59 +0530

    Jenkins Func Test

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -15,22 +15,22 @@ import org.testng.annotations.AfterTest;
 
 public class HelloTest {
 
-	/*WebDriver driver;
+	WebDriver driver;
 	WebElement text;
 	//@BeforeTest
 	public void beforeTest() {
-			System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();
+			/*System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
+		driver.get("http://localhost:8088/HelloWorld/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
 	//@Test
 	public void f() {
 
-		text = driver.findElement(By.xpath("html/body/h2"));
+		//text = driver.findElement(By.xpath("html/body/h2"));
 		Assert.assertEquals(driver.getTitle(), "");
 		System.out.println("Test Passed");
 
@@ -39,7 +39,7 @@ public class HelloTest {
 	//@AfterTest
 	public void afterTest() {
 		//driver.close();
-	}*/
+	}
 
 
 }
